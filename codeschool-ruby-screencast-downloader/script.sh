3e0edb63c597aa6d1a678af5dcc0a11d13239e04
Edoardo Morassutto <gianluigi988@gmail.com>
2015-02-28 22:39:05 +0100

    Added script for downloading the Ruby screencasts from codeschool.com

---
+++ codeschool-ruby-screencast-downloader/script.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# FIRST: create the cookies.txt file with the cookies from a codeschool page.
+# You must be enrolled to download pro content
+# You should use 'cookie.txt export' for chrome! and use the wget format
+
+# SECOND: load the index page with all the screencast about 'ruby'
+# search all the /screencasts/ url and tail off the unwanted ones
+curl -L https://www.codeschool.com/screencasts/all\?filters%5Bpath%5D\=ruby 2>/dev/null | \
+	egrep "/screencasts/[^\"]+" -o | \
+	head -n -2 | \
+	tail -n +9 > pages.txt
+
+# THIRD: reset the urls.txt file
+echo -n "" > urls.txt
+
+# FOURTH: foreach screencast load the page and search for the download url,
+# the episode name is the screencast url...
+while read path
+do
+	echo $path
+	url=$(wget --load-cookies=cookies.txt https://www.codeschool.com$path -O - 2>/dev/null | \
+		egrep -o "http://projector.codeschool.com/videos/[^'\"]+" | \
+		sed 's/\&amp;/\&/g' | \
+		head -n1)
+	name=$(echo $path | cut -b 14-)
+	echo "$name | $url" >> urls.txt
+done < pages.txt
+
+# FIFITH: prepare to download the episodes
+mkdir -p videos
+count=$(cat urls.txt | wc -l)
+i=1
+
+# SIXTH: download each episode and put them in the videos folder
+while read line
+do
+	name=$(echo $line | cut -d"|" -f 1 | tr -d '[[:space:]]')
+	url=$(echo $line | cut -d"|" -f 2 | tr -d '[[:space:]]')
+	echo "$i/$count Downloading videos/$name.mp4"
+	wget "$url" -O "videos/$name.mp4" -q
+	i=$((i+1))
+done < urls.txt
+
+# SEVENTH: DONE!
